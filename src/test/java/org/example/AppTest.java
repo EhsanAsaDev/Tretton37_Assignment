78d304d8fdbf884bf20db0a383cb9351f0255290
e.shakeri <QWEasd123>
2021-07-15 15:24:05 +0430

    write simple test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,33 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.example.model.DownloadList;
 import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void downLoad_google_dot_com() throws Exception {
+
+        String url = "https://www.google.com/";
+        Integer depth = 1;
+
+
+        WebsiteDownloader websiteDownloader = new WebsiteDownloader(url, depth);
+        DownloadList downloadList = websiteDownloader.run();
+
+        assertEquals(1, downloadList.getDownloadList().size());
+        assertEquals("https://www.google.com/", downloadList.getDownloadList().get(0));
+        assertEquals(6, downloadList.getUnDownloadList().size());
+        assertTrue(downloadList.getUnDownloadList().contains("https://www.google.com//advanced_search?hl=en-IR&authuser=0"));
     }
 }
